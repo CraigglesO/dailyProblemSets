3b040fcad9bdfc6c35d78c0ddf0d695f7c202654
CraigglesO <oconnorct1@gmail.com>
2019-05-21 22:04:06 -0400

    better comments

--- sorting/insertionsort/index.js
+++ sorting/insertionsort/index.js
@@ -5,11 +5,11 @@ function insertionSort (arr) {
   for (let i = 1, n = arr.length; i < n; i++) {
     let key = arr[i] // value in question, store it elsewhere, as this index value might be replaced
     let j = i - 1 // start comparisons right below the key
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j]
-      j--
+    while (j >= 0 && arr[j] > key) { // if the array index exists and the value is less than the key:
+      arr[j + 1] = arr[j] // move the value up
+      j-- // move down the array
     }
-    arr[j + 1] = key
+    arr[j + 1] = key // set the key in its new position
   }
   return arr
 }
