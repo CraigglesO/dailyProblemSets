3adb13b8b569eed974c3878229585c1d275e85c4
CraigglesO <oconnorct1@gmail.com>
2019-05-21 21:58:51 -0400

    insertionSort

--- sorting/insertionsort/README.md
+++ sorting/insertionsort/README.md
@@ -18,4 +18,11 @@ O(1)
 ```
 INSERTION_SORT(A)
 
+1 for i = 1 to n - 1
+2   j = i - 1
+3   key = A[i]
+4   while j >= 0 and A[j] > key
+5     arr[j + 1] = arr[j]
+6     j--
+7   arr[j + 1] = key
 ```
--- sorting/insertionsort/index.js
+++ sorting/insertionsort/index.js
@@ -1,34 +1,17 @@
 let arr = [33, 6, 21, 12, 19, 29, 38, 22, 14, 40]
 
 // This is a pick *last* element as pivot implementation
-function quickSort (arr, lo = 0, hi = arr.length - 1) {
-  if (lo < hi) {
-    let p = partition(arr, lo, hi) // find where to split (partition index)
-    quickSort(arr, lo, p - 1) // sort 'partitioned' lower array
-    quickSort(arr, p + 1, hi) // sort 'partitioned' upper array
+function insertionSort (arr) {
+  for (let i = 1, n = arr.length; i < n; i++) {
+    let key = arr[i] // value in question, store it elsewhere, as this index value might be replaced
+    let j = i - 1 // start comparisons right below the key
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j]
+      j--
+    }
+    arr[j + 1] = key
   }
   return arr
 }
 
-function partition (arr, lo, hi) {
-  let pivot = arr[hi]
-  let i = lo - 1
-  for (let j = lo; j < hi; j++) {
-    if (arr[j] <= pivot) {
-      i++
-      if (i !== j) {
-        let tmp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = tmp
-      }
-    }
-  }
-  i++
-  let tmp = arr[i]
-  arr[i] = arr[hi]
-  arr[hi] = tmp
-
-  return i
-}
-
-console.log(quickSort(arr))
+console.log(insertionSort(arr))
